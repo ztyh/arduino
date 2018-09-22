56b780c0d43c66a86cc6b885c5a0f196afb5a012
ztyh <ztyh0121@gmail.com>
2018-09-22 18:14:38 -0400

    added project 11

--- 11/11.ino
+++ 11/11.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(l2,l1,5,4,3,2);
-const int switchPin 6;
+LiquidCrystal lcd(12,11,5,4,3,2);
+const int switchPin=6;
 int switchState=0;
 int prevSwitchState=0;
 int reply;
@@ -23,28 +23,28 @@ void loop(){
       lcd.print("The ball says:");
       lcd.setCursor(0,1);
       switch(reply){
-        case 0;
+        case 0:
           lcd.print("Yes");
           break;
-        case 1;
+        case 1:
           lcd.print("Most likely");
           break;
-        case 2;
+        case 2:
           lcd.print("Certainly");
           break;
-        case 3;
+        case 3:
           lcd.print("Outlook good");
           break;
-        case 4;
+        case 4:
           lcd.print("Unsure");
           break;
-        case 5;
+        case 5:
           lcd.print("Ask again");
           break;
-        case 6;
+        case 6:
           lcd.print("Doubtful");
           break;
-        case 7;
+        case 7:
           lcd.print("No");
           break;
       }
